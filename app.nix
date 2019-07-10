856d0d8f5ee1509fd2c8c60dc03e392455524ab6
Travis Athougies <travis@athougies.net>
2019-07-09 22:52:21 -0700

    Switch to rspamd

--- app.nix
+++ app.nix
@@ -1,14 +1,15 @@
 { pkgs, lib, intrustd, pure-build, ... }:
 let ourPkgs = (import ./shell.nix { inherit pkgs; });
-    inherit (ourPkgs) exim clamav spamassassin;
+    inherit (ourPkgs) exim clamav;
 
     mailDir = "/intrustd/mail";
     spoolDir = "${mailDir}/spool";
     intrustdMailDb = "/intrustd/mail/accounts.db";
-    spamAssassinStateDir = "/intrustd/spam";
     clamAvDir = "/run/clamav";
     clamAvStateDir = "/intrustd/clamav-db";
-    spamdSock = "/run/spamd.sock";
+    spamdPort = 11333;
+    spamdControllerPort = spamdPort + 1;
+    spamdPid = "/run/spamd.pid";
 
     accountsDir = "/intrustd/accounts";
 
@@ -19,14 +20,14 @@ let ourPkgs = (import ./shell.nix { inherit pkgs; });
     eximGroup = "intrustd";
     clamAvUser = "intrustd";
     clamAvGroup = "intrustd";
-    spamAssassinUser = "intrustd";
-    spamAssassinGroup = "intrustd";
+    rspamdUser = "intrustd";
+    rspamdGroup = "intrustd";
 
     eximConfiguration = pkgs.substituteAll {
       isExecutable = false;
       src = ./resources/exim.conf;
       inherit mailDir spoolDir intrustdMailDb clamAvSock
-              spamdSock eximUser eximGroup accountsDir;
+              spamdPort eximUser eximGroup accountsDir;
     };
 
     clamAvConfiguration = pkgs.substituteAll {
@@ -35,26 +36,12 @@ let ourPkgs = (import ./shell.nix { inherit pkgs; });
       inherit clamAvSock clamAvPid clamAvUser clamAvGroup clamAvStateDir;
     };
 
-    spamd-init-pre = pkgs.writeText "init.pre" ''
-      required_score   5.0
-      use_bayes        1
-      bayes_auto_learn 1
-      add_header all Status _YESNO_, score=_SCORE_ required=_REQD_ tests=_TESTS_ autolearn=_AUTOLEARN_ version=_VERSION_
-    '';
-
-    spamd-local-cf = pkgs.substituteAll {
+    rspamdConfiguration = pkgs.substituteAll {
       isExecutable = false;
-      src = ./resources/spamd-local.cf;
+      src = ./resources/rspamd.conf;
+      inherit spamdPort spamdPid spamdControllerPort;
     };
 
-    spamdEnv = pkgs.buildEnv {
-      name = "spamd-env";
-      paths = [];
-      postBuild = ''
-        ln -sf ${spamd-init-pre} $out/init.pre
-        ln -sf ${spamd-local-cf} $out/local.cf
-      '';
-    };
 in {
   app.meta = {
     slug = "mail";
@@ -89,16 +76,12 @@ in {
     '';
   };
 
-  app.services.spamassassin = {
-    name = "spamassassin";
+  app.services.rspamd = {
+    name = "rspamd";
     autostart = true;
 
     startExec = ''
-       mkdir -p ${spamAssassinStateDir}
-       chown ${spamAssassinUser}:${spamAssassinGroup} ${spamAssassinStateDir}
-       # TODO Run sa-update
-       exec ${spamassassin}/bin/spamd --username=${spamAssassinUser} --groupname=${spamAssassinGroup} \
-            --siteconfigpath=${spamdEnv} --allow-tell --pidfile=/run/spamd.pid --virtual-config-dir=${accountsDir}/%u/spam
+       exec ${pkgs.rspamd}/bin/rspamd --user=${rspamdUser} --group=${rspamdGroup} -f -c ${rspamdConfiguration}
     '';
   };
 
